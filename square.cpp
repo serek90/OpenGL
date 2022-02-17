d87d0b01e958d7f21b0e1fb34ab2c2e88439f4c7
Jakub Seroczynski <43785920+serek90@users.noreply.github.com>
2022-02-17 19:37:47 +0100

    add two small program to draw square and triangle

---
+++ square.cpp
@@ -0,0 +1,66 @@
+#include <stdlib.h>
+#define GLEW_STATIC
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+#include <iostream>
+
+int width = 1080;
+int height = 720;
+
+void drawSquare()
+{
+	/* the order metters!!! */
+	glBegin(GL_QUADS);
+	glColor3f(1, 0, 0);
+	glVertex3f(-0.5, -0.5, 0);
+
+	glColor3f(1, 0, 0);
+	glVertex3f(0.5, -0.5, 0);
+
+	glColor3f(1, 0, 0);
+	glVertex3f(0.5, 0.5, 0);
+
+	glColor3f(1, 0, 0);
+	glVertex3f(-0.5, 0.5, 0);
+
+
+	glEnd();
+}
+
+int main(int argc, char* argv[])
+{
+	std::cout << "Start drawing square" << std::endl;
+
+	if (!glfwInit()) exit(EXIT_FAILURE);
+
+	GLFWwindow* window;
+	window = glfwCreateWindow(width, height, "glfw", NULL, NULL);
+	if (!window)
+	{
+		glfwTerminate();
+		exit(EXIT_FAILURE);
+	}
+	glfwMakeContextCurrent(window);
+	std::cout << "Window is created" << std::endl;
+
+	/* main loop */
+	while (!glfwWindowShouldClose(window))
+	{
+		glfwGetFramebufferSize(window, &width, &height);  // the window can resize
+		glViewport(0, 0, width, height);
+		glClearColor(.2, .2, .2, 1); //set background Color to dark grey
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		drawSquare ();
+
+		glfwSwapBuffers(window);  //swap buffers
+		glfwPollEvents();         //listen for window events
+	}
+
+	// Destroy window and terminate glfw
+	glfwDestroyWindow(window);
+	glfwTerminate();
+	std::cout << "Window was destroyed" << std::endl;
+
+	return 0;
+}
