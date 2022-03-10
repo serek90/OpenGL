c84411b55e98d5e3eccb78ae253e189bb6a3a22c
serek90 <43785920+serek90@users.noreply.github.com>
2022-03-10 17:37:50 +0100

    Draw triangle witch color changing using uniform in shader

---
+++ (9)Shaders-uniforms_color_change/color_change_uniforms.cpp
@@ -0,0 +1,164 @@
+/*
+* My solution to exercise from page:
+* https://learnopengl.com
+*
+* Draw triangle which change its color
+* First program using uniforms in shaders
+* 
+* @author: jseroczy (serek90)
+*/
+
+/* standard libs */
+#include <stdlib.h>
+#include <iostream>
+
+/* OpenGL libs */
+#define GLEW_STATIC
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+
+/* window sizes */
+int width = 1080;
+int height = 720;
+
+float vertices[] = {
+	-0.5f, -0.5f, 0.0f,
+	 0.5f, -0.5f, 0.0f,
+	 0.0f,  0.5f, 0.0f
+};
+
+const char* vertexShaderSource = "#version 330 core\n"
+"layout (location = 0) in vec3 aPos;\n"
+"void main()\n"
+"{\n"
+"   gl_Position = vec4(aPos, 1.0);\n"
+"}\0";
+
+const char* fragmentShaderSource = "#version 330 core\n"
+"out vec4  FragColor;"
+"uniform vec4 shaderColor;"
+"void main()\n"
+"{\n"
+"	FragColor = shaderColor;\n"
+"}\0";
+
+void prepareShader(void);
+unsigned int VAO;
+unsigned int VBO;
+unsigned int shaderProgram;
+
+int main(int argc, char* argv[])
+{
+	std::cout << "Start drawing triangle" << std::endl;
+	glewExperimental = GL_TRUE;
+	if (!glfwInit()) exit(EXIT_FAILURE);
+
+	GLFWwindow* window;
+	window = glfwCreateWindow(width, height, "glfw", NULL, NULL);
+	if (!window)
+	{
+		std::cout << "glfwCreateWindow";
+		glfwTerminate();
+		exit(EXIT_FAILURE);
+	}
+	glfwMakeContextCurrent(window);
+	std::cout << "Window is created" << std::endl;
+
+	GLint GlewInitResult = glewInit();
+	if (GLEW_OK != GlewInitResult)
+	{
+		printf("ERROR: %s", glewGetErrorString(GlewInitResult));
+		exit(EXIT_FAILURE);
+	}
+
+	prepareShader();
+	
+	/* main loop */
+	while (!glfwWindowShouldClose(window))
+	{
+		glfwGetFramebufferSize(window, &width, &height);  // the window can resize
+		glViewport(0, 0, width, height);
+		glClearColor(.2, .2, .2, 1); //set background Color to dark grey
+		glClear(GL_COLOR_BUFFER_BIT);
+		glUseProgram(shaderProgram);
+
+		float timeValue = glfwGetTime();
+		float blueValue = sin(timeValue) / 2.0f + 0.5f;
+		int vertexColorLocation = glGetUniformLocation(shaderProgram, "shaderColor");
+		glUniform4f(vertexColorLocation, 0.0f, 0.0f, blueValue, 1.0f);
+
+		glBindVertexArray(VAO);
+		glDrawArrays(GL_TRIANGLES, 0, 3);
+
+		glfwSwapBuffers(window);  //swap buffers
+		glfwPollEvents();         //listen for window events
+	}
+
+	// Destroy window and terminate glfw
+	glfwDestroyWindow(window);
+	glfwTerminate();
+	std::cout << "Window was destroyed" << std::endl;
+	
+	return 0;
+}
+
+void prepareShader(void)
+{
+	/* check variables */
+	int  success;
+	char infoLog[512];
+
+	/* vertex shader */
+	unsigned int vertexShader;
+	vertexShader = glCreateShader(GL_VERTEX_SHADER);
+	glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+	glCompileShader(vertexShader);
+	glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+	if (!success)
+	{
+		glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+		std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
+	}
+
+	/* fragment shader compilation */
+	unsigned int fragmentShader;
+	fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+	glCompileShader(fragmentShader);
+	glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+	if (!success)
+	{
+		glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
+		std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
+	}
+
+	/* shader program creation */
+	shaderProgram = glCreateProgram();
+	glAttachShader(shaderProgram, vertexShader);
+	glAttachShader(shaderProgram, fragmentShader);
+	glLinkProgram(shaderProgram);
+	glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+	if (!success) {
+		glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
+	}
+	glUseProgram(shaderProgram);
+
+	/* delete unnecessary shaders */
+	glDeleteShader(vertexShader);
+	glDeleteShader(fragmentShader);
+
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
+
+	glGenBuffers(1, &VBO);
+	glGenVertexArrays(1, &VAO);
+	// 1. bind Vertex Array Object
+	glBindVertexArray(VAO);
+	// 2. copy our vertices array in a buffer for OpenGL to use
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+	// 3. then set our vertex attributes pointers
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
+
+}
