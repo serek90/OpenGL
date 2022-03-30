24320b617abf54ef31ea9f35539d09d4454eb744
serek90 <43785920+serek90@users.noreply.github.com>
2022-03-30 20:36:07 +0200

    rotate simple square

--- (20)square_rotate/square_rotate.cpp
+++ (20)square_rotate/square_rotate.cpp
@@ -2,53 +2,55 @@
 * My solution to exercise from page:
 * https://learnopengl.com
 *
-* Draw triangle using shaders and make different color in every corner
+* using textuers in OpenGL example
 *
-* copy glm from : https://glm.g-truc.net/0.9.8/index.html
-* 
+* stb lib from: https://github.com/nothings/stb
+* GLM libs from: https://glm.g-truc.net/0.9.8/index.html
+*
 * @author: jseroczy (serek90)
 */
 
 /* standard libs */
 #include <stdlib.h>
 #include <iostream>
-
-
 /* OpenGL libs */
 #define GLEW_STATIC
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 #include "Shader.h"
-
-/* open GL mathematics */
+/* stb_lib.h */
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb/stb_image.h"
+/* GLM libs */
 #include "glm/glm.hpp"
 #include "glm/gtc/matrix_transform.hpp"
 #include "glm/gtc/type_ptr.hpp"
 
 /* window sizes */
-int width = 1080;
-int height = 720;
+int width = 1100;
+int height = 800;
 
 float vertices[] = {
-	 /* positions  */       /* colors  */
-	-0.5f, -0.5f, 0.0f,   1.0f, 0.0f, 0.0f, // bottom right 
-	 0.5f, -0.5f, 0.0f,   0.0f, 1.0f, 0.0f, // bottom left
-	 0.0f,  0.5f, 0.0f,   0.0f, 0.0f, 1.0f  // top
+	// positions          // colors           // texture coords
+	 0.5f,  0.5f, 0.0f,   1.0f, 0.0f, 0.0f,   1.0f, 1.0f,   // top right
+	 0.5f, -0.5f, 0.0f,   0.0f, 1.0f, 0.0f,   1.0f, 0.0f,   // bottom right
+	-0.5f, -0.5f, 0.0f,   0.0f, 0.0f, 1.0f,   0.0f, 0.0f,   // bottom left
+	-0.5f,  0.5f, 0.0f,   1.0f, 1.0f, 0.0f,   0.0f, 1.0f    // top left 
+};
+unsigned int indices[] = {
+	0, 1, 3, // first triangle
+	1, 2, 3  // second triangle
 };
 
 void prepareShader(void);
+void prepereTexture(void);
 unsigned int VAO;
 unsigned int VBO;
+unsigned int EBO;
+unsigned int texture;
 
 int main(int argc, char* argv[])
 {
-	
-	//glm::vec4 vec(5.0f, 2.0f, 0.0f, 1.0f);
-	//glm::mat4 trans = glm::mat4(1.0f);
-	//trans = glm::translate(trans, glm::vec3(1.0f, 1.0f, 0.0f)); // 
-	//vec = trans * vec;
-
-	//std::cout <<  "x = " << vec.x << " y = " << vec.y << " z = " << vec.z << std::endl;
 	std::cout << "Start drawing triangle" << std::endl;
 	glewExperimental = GL_TRUE;
 	if (!glfwInit()) exit(EXIT_FAILURE);
@@ -72,7 +74,9 @@ int main(int argc, char* argv[])
 	}
 
 	Shader myShader("shader.vs", "shader.fs");
+	prepereTexture();
 	prepareShader();
+
 	
 	/* main loop */
 	while (!glfwWindowShouldClose(window))
@@ -81,19 +85,18 @@ int main(int argc, char* argv[])
 		glViewport(0, 0, width, height);
 		glClearColor(.2, .2, .2, 1); //set background Color to dark grey
 		glClear(GL_COLOR_BUFFER_BIT);
-		
-		//glm::mat4 trans = glm::mat4(1.0f);
-		//trans = glm::rotate(trans, glm::radians(0.0f), glm::vec3(0.0, 0.0, 1.0));
-		//trans = glm::scale(trans, glm::vec3(0.5, 0.5, 0.5));
+
 		glm::mat4 trans = glm::mat4(1.0f);
 		trans = glm::translate(trans, glm::vec3(0.5f, -0.5f, 0.0f));
 		trans = glm::rotate(trans, (float)glfwGetTime(), glm::vec3(0.0f, 0.0f, 1.0f));
-
 		unsigned int transformLoc = glGetUniformLocation(myShader.getID(), "transform");
 		glUniformMatrix4fv(transformLoc, 1, GL_FALSE, glm::value_ptr(trans));
+
 		myShader.use();
+
+		glBindTexture(GL_TEXTURE_2D, texture);
 		glBindVertexArray(VAO);
-		glDrawArrays(GL_TRIANGLES, 0, 3);
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
 		glfwSwapBuffers(window);  //swap buffers
 		glfwPollEvents();         //listen for window events
@@ -110,20 +113,54 @@ int main(int argc, char* argv[])
 //JSJS TODO: Change name
 void prepareShader(void)
 {
-
 	glGenBuffers(1, &VBO);
 	glGenVertexArrays(1, &VAO);
+	glGenBuffers(1, &EBO);
+	glGenVertexArrays(1, &EBO);
 	// 1. bind Vertex Array Object
 	glBindVertexArray(VAO);
 	// 2. copy our vertices array in a buffer for OpenGL to use
+
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 	// 3. then set our vertex attributes pointers
 	/* position attribute */
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0);
 	glEnableVertexAttribArray(0);
 	/* color attribute */
-	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(3 * sizeof(float)));
 	glEnableVertexAttribArray(1);
+	/* texture coridinations */
+	glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(6 * sizeof(float)));
+	glEnableVertexAttribArray(2);
 
+}
+
+void prepereTexture(void)
+{
+	glGenTextures(1, &texture);
+	glBindTexture(GL_TEXTURE_2D, texture);
+	// set the texture wrapping/filtering options (on the currently bound texture object)
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+	/* Load image */
+	int width, height, nrChannels;
+	unsigned char* data = stbi_load("container.jpg", &width, &height, &nrChannels, 0);
+	if (data)
+	{
+		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+		glGenerateMipmap(GL_TEXTURE_2D);
+	}
+	else
+	{
+		std::cout << "Failed to load texture" << std::endl;
+	}
+
+	stbi_image_free(data);
 }
